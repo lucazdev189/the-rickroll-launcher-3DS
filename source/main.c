34ba80a2f52863dcde22863f22e313009d07305b
lucazdev189 <101880773+lucazdev189@users.noreply.github.com>
2022-03-18 23:12:16 +0800

    Add files via upload

---
+++ source/main.c
@@ -0,0 +1,63 @@
+/*
+	This is the work of lucazdev189. Credit to original author Aurelio Mannara.
+    In this program, I am going to show you how to do a simple rickroll using
+	only printf.
+*/
+
+#include <3ds.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	
+	gfxInitDefault();
+
+	
+	PrintConsole topScreen, bottomScreen;
+
+	
+	consoleInit(GFX_TOP, &topScreen);
+	consoleInit(GFX_BOTTOM, &bottomScreen);
+
+	
+	consoleSelect(&topScreen);
+	printf("\x1b[2;20HNever Gonna\n");
+
+	
+	consoleSelect(&bottomScreen);
+	printf("\x1b[2;20HGive you up\n");
+
+	consoleSelect(&topScreen);
+	printf("\x1b[25;20HPress Start to exit this rickroll.");
+
+	consoleSelect(&topScreen);
+	printf("\x1b[30;20Hlucazdev189.");
+
+	// Main loop
+	while (aptMainLoop())
+	{
+		
+		hidScanInput();
+
+		
+		u32 kDown = hidKeysDown();
+
+		if (kDown & KEY_START) break; 
+
+		
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+
+		
+		gspWaitForVBlank();
+	}
+
+	
+	gfxExit();
+
+	return 0;
+}
+
+// Made with devkitpro. Just experimenting around the examples. First project done!
+
+// lucazdev189
